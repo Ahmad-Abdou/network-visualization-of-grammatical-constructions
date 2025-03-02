aae678f23e3688362ece6e501899fc3f016c2a3b
Ahmad Abdou <aa224rg@student.lnu.se>
2025-03-02 07:08:37 +0100

    Fixing the height and width and the zoom and panning to the nodes

--- js/Home.js
+++ js/Home.js
@@ -12,13 +12,11 @@ class Home {
     this.timeValues = null
     this.frequenciesValues = null
     this.sliderValue = null
-    this.svg.call(d3.zoom().on('zoom', (event) => {
-      this.g.attr('transform', event.transform)
-    }))
+    this.numberOfFiles = 0
+    this.zoomGroups = new Map()
   }
-
-  buildHierarchyFromCSV_5_Verbs(csvData) {
-    let root = { name: "root", children: [] };
+  buildHierarchyFromCSV_5_Verbs(csvData, file) {
+    let root = { name: file.slice(7, file.length - 4), children: [] };
     csvData.forEach(row => {
       const columns = Object.keys(row)
       let current = root;
@@ -45,135 +43,13 @@ class Home {
   buildHirearchy(file) {
     d3.csv(file).then((csvData) => {
     const treeLayout = d3.tree().size([this.innerHeight, this.innerWidth])
-    const nestedData = this.buildHierarchyFromCSV_5_Verbs(csvData);
+    const nestedData = this.buildHierarchyFromCSV_5_Verbs(csvData, file);
+    this.numberOfFiles++
     const root = d3.hierarchy(nestedData)
     this.collapsable(root)
-
-  //   const links = treeLayout(root).links()
-  //   const linkPathGenerator = d3.linkHorizontal().x(d => d.y).y(d => d.x)
-
-  //   const frequencyExtent = d3.extent(links, d=> d.target.data.frequency)
-  //   const StrokeScale = d3.scaleLinear().domain(frequencyExtent).range([1, 5])
-  //   const opacityScale = d3.scaleLinear().domain(frequencyExtent).range([0.5, 1])
-
-  //   const timeScale = d3.extent(links, d=> d.target.data.year)
-
-  //   const slider = document.querySelector('.filter-slide')
-  //   slider.min = timeScale[0]
-  //   slider.max = timeScale[1]
-  //   slider.value = timeScale[0]
-
-  //   const filterValue = document.querySelector('.filter-value')
-  //   filterValue.textContent = timeScale[0]
-
-  //   let filterPathByYear = links
-  //   let filterNodesByYear = root.descendants()
-  //   self = this
-    
-  //   const renderVisualization = (pathData, nodeData) => {
-  //   self.currentPathData = pathData;
-  //   self.currentNodeData = nodeData;
-
-  //   self.g.selectAll('path').remove();
-  //   self.g.selectAll('circle').remove();
-    
-  //   try {
-  //       self.g.selectAll('path')
-  //           .data(pathData)
-  //           .join('path')
-  //           .attr('d', linkPathGenerator)
-  //           .attr('fill', 'none')
-  //           .attr('stroke', 'black')
-  //           .attr('stroke-width', d => StrokeScale(d.target.data.frequency))
-  //           .attr('stroke-opacity', d => opacityScale(d.target.data.frequency));
-
-  //       const nodes = self.g.selectAll('circle')
-  //           .data(nodeData)
-  //           .join('circle')
-  //           .attr('class', 'nodes')
-  //           .attr('cx', d => d.y)
-  //           .attr('cy', d => d.x)
-  //           .attr('r', d => Math.max(12 - d.depth * 2, 3) + 'px');
-
-  //       self.toolTipsImpl(nodes);
-  //   } catch (error) {
-  //       console.error('Render error:', error);
-  //       if (self.lastGoodPathData && self.lastGoodNodeData) {
-  //           renderVisualization(self.lastGoodPathData, self.lastGoodNodeData);
-  //       }
-  //   }
-  //   self.lastGoodPathData = pathData;
-  //   self.lastGoodNodeData = nodeData;
-  // }
-
-  // this.Debouncer(renderVisualization, slider, filterValue, filterPathByYear, filterNodesByYear, links, root)
-
-  // renderVisualization(filterPathByYear, filterNodesByYear);
   })
-}
-
-  toolTipsImpl(nodes) {
-    const toolTips = this.g.append('g').attr('class', 'tooltips').style('display', 'none')
-    toolTips.append('rect')
-    .attr('class', 'rect-tooltips')
-    .attr('width', 150)
-    .attr('height', 90)
-    .style('fill', '#0080FF')
-    .attr('rx', '10')
-    .attr('ry', '10')
-    .attr('y', '-40')
-    .attr('x', '-60')
-
-    toolTips.append('text')
-    .attr('font-size', '12px')
-    .attr('color', 'red')
-
-    nodes.on('click', function (event, d) {
-    const children = event.target.__data__.descendants()
-  
-    d3.selectAll('circle')
-    .attr('fill', 'black')
-    .attr('opacity', '1')
-
-
-    d3.selectAll('circle')
-    .filter(n => children.includes(n))
-    .attr('fill', 'crimson');
-  
-  d3.selectAll('circle')
-    .filter(n => !children.includes(n) && n !== d)
-    .attr('opacity', '0.1');
-
-    })
-
-    nodes.on('mouseover', function() {
-      toolTips.style('display', 'block')     
-    }).on('mousemove', function(event, data) {
-      const [x,y] = d3.pointer(event)
-      toolTips.attr('transform', `translate(${x - 50},${y - 60})`);
-      toolTips.selectAll('#tooltip-text').remove();
-
-      toolTips.selectAll('text')
-      .attr('id', 'tooltip-text')
-      .data([`Name: ${data.data.name}`,
-            `Year: ${data.data.year}`,
-            `Frequency: ${data.data.frequency}`])
-      .join('text')
-      .attr('x', 20)
-      .attr('y', (d,i) => (30 * i) -20)
-      .attr('text-anchor', 'middle')
-      .text(d=>d)
-
-      toolTips.style('display', 'block')     
-    }).on('mouseout', function() {
-      toolTips.selectAll('#tooltip-text').remove()
-      toolTips.style('display', 'none')     
-    })
   }
-
   collapsable(root) {
-
-  self = this
 
   const dx = 15;
   const dy = (this.width - this.margin.right - this.margin.left) / (1 + root.height);
@@ -181,22 +57,40 @@ class Home {
   const tree = d3.tree().nodeSize([dx, dy]);
   const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
 
-      self.svg
-      .attr("viewBox", [-this.margin.left, -this.margin.top, this.width, this.height])
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; user-select: none;");
+  const container = this.svg.append('g')
+      .attr('class', `visualization-container-${this.numberOfFiles}`);
 
-  const gLink = this.svg.append("g")
+    container
+      .attr("viewBox", [-this.margin.left, -this.margin.top, this.width, this.height])
+      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; user-select: none;")
+      .attr('transform', 'translate(50, 100)')
+
+  const gLink = container.append("g")
       .attr("fill", "none")
       .attr("stroke", "#555")
       .attr("stroke-opacity", 0.4)
       .attr("stroke-width", 1.5);
 
-  const gNode = this.svg.append("g")
+  const gNode = container.append("g")
       .attr("cursor", "pointer")
       .attr("pointer-events", "all");
 
+    const zoomG = container.append('g')
+    .attr('id',`zoom-group-${this.numberOfFiles}`)
+    .attr('class',`zoom-group`)
 
-  function update(event, source) {
+    zoomG.append(() => gLink.node())
+    zoomG.append(() => gNode.node())
+
+    const zoom = d3.zoom()
+    .on('zoom', (event) => {
+      container.select('g').attr('transform', event.transform)
+    })
+    this.zoomGroups.set(this.numberOfFiles, container);
+
+    container.call(zoom)
+
+  const update = (event, source) =>  {
     const nodes = root.descendants().reverse();
     const links = root.links();
 
@@ -206,27 +100,27 @@ class Home {
     let right = root;
     let top = root;
     let bottom = root;
-    const min_width = self.width
-    const min_height = self.height
+    const min_width = d3.select('.tree-container').style('width')
+    const min_height = d3.select('.tree-container').style('height')
 
     root.eachBefore(node => {
       if (node.x < left.x) left = node;
       if (node.x > right.x) right = node;
     });
 
-    const height = Math.max(right.x - left.x + self.margin.top + self.margin.bottom, min_height);
-    const width = Math.max(bottom.y - top.y + self.margin.left + self.margin.right, min_width);
+    const height = Math.max(right.x - left.x + this.margin.top + this.margin.bottom, min_height);
+    const width = Math.max(bottom.y - top.y + this.margin.left + this.margin.right, min_width);
 
-    const transition = self.svg.transition()
+    const transition = container.transition()
         .duration(750)
         .attr("height", height)
         .attr("viewBox", [
-          -self.margin.left,            
-          left.x - self.margin.top,
+          -this.margin.left,            
+          left.x - this.margin.top,
           Math.max(width, min_width),
           Math.max(height, min_height)
       ])
-        .tween("resize", window.ResizeObserver ? null : () => () => self.svg.dispatch("toggle"));
+        .tween("resize", window.ResizeObserver ? null : () => () => this.svg.dispatch("toggle"));
 
     const node = gNode.selectAll("g")
       .data(nodes, d => d.id);
@@ -298,10 +192,19 @@ class Home {
   });
 
   update(null, root);
-
-  return self.svg.node();
+  this.rightClick(container)
+  return this.svg.node();
   }
+  rightClick(container) {
+    console.log(container)
+    container.on('contextmenu',(e) => {
+      e.preventDefault()
+      const menu = document.createElement('div')
+      menu.style.width = '70px'
+      menu.style.height = '80px'
 
+    })
+  }
   Debouncer(renderVisualization, slider, filterValue, filterPathByYear, filterNodesByYear, links, root) {
     function debounce(func, wait) {
       let timeout;
@@ -320,14 +223,14 @@ class Home {
   }, 100);
 
   slider.addEventListener('input', (e) => {
-      self.sliderValue = e.target.value;
+      this.sliderValue = e.target.value;
       filterValue.textContent = e.target.value;
       
       filterPathByYear = links.filter(
-          link => Number(link.target.data.year) >= Number(self.sliderValue)
+          link => Number(link.target.data.year) >= Number(this.sliderValue)
       );
       filterNodesByYear = root.descendants().filter(
-          row => Number(row.data.year) >= Number(self.sliderValue)
+          row => Number(row.data.year) >= Number(this.sliderValue)
       );
       debouncedRender(filterPathByYear, filterNodesByYear);
   });
