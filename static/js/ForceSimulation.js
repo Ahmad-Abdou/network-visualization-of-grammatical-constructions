86be5a4f0ec582d40dc66e11e40d33c8d9663655
Ahmad Abdou <aa224rg@student.lnu.se>
2025-03-27 14:34:04 +0100

    adding functionality button to show all degrees

--- static/js/ForceSimulation.js
+++ static/js/ForceSimulation.js
@@ -29,29 +29,6 @@ class ForceSimulation {
       .force("link", d3.forceLink(links).id(d => d.name).distance(50).strength(1))
       .force("charge", d3.forceManyBody().distanceMin(200).distanceMax(1000).strength(-1))
       .stop()
-      // .force("x", d3.forceX(this.width / 2))
-      // .force("y", d3.forceY(this.height / 2));
-
-      const workerCode = `
-      self.onmessage = function(event) {
-        const data = event.data
-
-      const nodes = root.nodes
-      const links = root.links
-      const simulation = d3.forceSimulation(nodes)
-      .force("collide", d3.forceCollide(30).iterations(20))
-      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-      .force("link", d3.forceLink(links).id(d => d.name).distance(50).strength(1))
-      .force("charge", d3.forceManyBody().distanceMin(20).distanceMax(100).strength(-10))
-
-      const numIterations = nodes.length > 100 ? 100 : 300
-      for (let i = 0; i < numIterations; i++) {
-        simulation.tick();
-      }
-    }
-
-      `
-      
   
       const numIterations = nodes.length > 100 ? 100 : 300
       for (let i = 0; i < numIterations; i++) {
@@ -69,23 +46,75 @@ class ForceSimulation {
       .attr("x2", d => d.target.x)
       .attr("y2", d => d.target.y);
   
-    // const node = this.nodeGroup.append("g")
-    //   .attr("fill", "crimson")
-    //   .selectAll("circle")
-    //   .data(nodes)
-    //   .join("circle")
-    //   .attr("fill", d => d.children ? null : "#17B169")
-    //   .attr("stroke", d => d.children ? null : "#fff")
-    //   .attr("r", d => d.children ? 5 : 3.5)
-    //   .call(this.drag(simulation))
-    const node = this.nodeGroup.append("g")
+    const selected_option_degree = document.querySelector('.degree-selection')
+    selected_option_degree.addEventListener('change', () => {
+      d3.select('#nodes').remove()
+      const node = this.nodeGroup.append("g")
+      .attr('id', 'nodes')
       .attr("fill", "crimson")
       .selectAll("circle")
       .data(nodes)
       .join("circle")
       .attr("fill", d => this.calculateNodeColor(d))
       .attr("stroke", d => d.children ? null : "#fff")
-      .attr("r", d => this.calculateInDegree(d))
+      .attr("r", (d) => {
+        if (selected_option_degree.value === 'degree'){
+          return this.calculateDegree(d)
+        }else if (selected_option_degree.value === 'in_degree'){
+          return this.calculateInDegree(d)
+        }else if (selected_option_degree.value === 'out_degree'){
+          return this.calculateOutDegree(d)
+        }
+      } )
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y)
+      .call(this.drag(simulation));
+      
+
+      node.append("title")
+        .text(d => d.name);
+
+      simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+    
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      });
+
+      this.nodeGroup.call(d3.zoom()
+      .extent([[0, 0], [ this.width,  this.height]])
+      .scaleExtent([-5, 8])
+      .on("zoom", zoomed));
+
+      function zoomed({transform}) {
+        node.attr("transform", transform);
+        link.attr("transform", transform);
+
+      }
+    })
+    const node = this.nodeGroup.append("g")
+      .attr('id', 'nodes')
+      .attr("fill", "crimson")
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+      .attr("fill", d => this.calculateNodeColor(d))
+      .attr("stroke", d => d.children ? null : "#fff")
+      .attr("r", (d) => {
+        if (selected_option_degree.value === 'degree'){
+          return this.calculateDegree(d)
+        }else if (selected_option_degree.value === 'in_degree'){
+          return this.calculateInDegree(d)
+        }else if (selected_option_degree.value === 'out_degree'){
+          return this.calculateOutDegree(d)
+        }
+      } )
+      
       .attr("cx", d => d.x)
       .attr("cy", d => d.y)
       .call(this.drag(simulation));
@@ -164,7 +193,6 @@ class ForceSimulation {
     const maxValue = Math.max(...values);
   
     const scale = d3.scaleLinear().domain([minValue, maxValue]).range([3, 25]);
-    console.log(`${nodeDegree} :  ${scale(nodeDegree)}`)
     return scale(nodeDegree);
   }
   calculateInDegree(node){
